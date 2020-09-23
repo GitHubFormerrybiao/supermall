50764ff268678074c111ffb3909ae94089d1b70c
wurenbiao <wurb@huaxindata.com.cn>
2020-09-23 17:50:43 +0800

    增加本周流行

--- src/views/home/childComps/RecommendView.vue
+++ src/views/home/childComps/RecommendView.vue
@@ -33,7 +33,8 @@ export default {
   width: 100%;
   text-align: center;
   font-size: 12px;
-  padding: 15px 0 30px;
+  padding: 10px 0 20px;
+  border-bottom: 10px solid #eee;
 }
 .recommend-item {
   flex: 1;
